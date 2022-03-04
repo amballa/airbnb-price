2a8071f74ad50e7ca15cc1adeb061ab35bf3bd7f
Abdel Balla <92558174+amballa@users.noreply.github.com>
2022-03-04 11:54:20 -0800

    adding model and output

--- project/app.py
+++ project/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request
 from flask_assets import Bundle, Environment
-from tables import Description
+from .model import user_desc
+import pickle
+import pandas as pd
+import numpy as np
 
 
 def build_app():
 
     app = Flask(__name__)
-
 
     js = Bundle('breakpoints.min.js', 'browser.min.js', 'jquery.dropotron.min.js',
                 'jquery.min.js', 'jquery.scrollex.min.js', 'main.js', 'util.js',
@@ -26,26 +28,35 @@ def build_app():
     @app.route('/#')
     def return_to_home():
         return render_template('index.html')
-    
+
     @app.route('/results', methods=['POST'])
     def display_results():
-        capacity = request.values['capacity']
-        bedrooms = request.values['bedrooms']
-        beds = request.values['beds']
-        baths = request.values['baths']
-        min_nights = request.values['minimum_nights']
-        max_nights = request.values['maximum_nights']
-        amnt_amenities = request.values['amnt_amenities']
-        room_type = request.values['room_type']
+        capacity = int(request.values['capacity'])
+        bedrooms = int(request.values['bedrooms'])
+        beds = int(request.values['beds'])
+        baths = int(request.values['baths'])
+        min_nights = int(request.values['minimum_nights'])
+        max_nights = int(request.values['maximum_nights'])
+        amnt_amenities = int(request.values['amnt_amenities'])
+        room_type = int(request.values['room_type'])
         desc = request.values['desc']
-        return render_template('results.html', 
-                                capacity=capacity,
-                                bedrooms=bedrooms,
-                                beds=beds,
-                                baths=baths,
-                                min_nights=min_nights,
-                                max_nights=max_nights,
-                                amnt_amenities=amnt_amenities,
-                                room_type=room_type,
-                                desc=desc)
+
+        user_arr = np.array([room_type, capacity, bedrooms,
+                            beds, min_nights, max_nights, amnt_amenities, baths])
+
+        df_nlp = user_desc(desc)
+        arr_nlp = df_nlp.to_numpy()
+
+        user_arr = np.concatenate((user_arr, arr_nlp))
+        user_arr_reshaped = user_arr.reshape(1, len(user_arr))
+
+        # Model
+        model = pickle.load(open('best_regressor', 'rb'))
+
+        result = model.predict(user_arr_reshaped)
+
+        price = result[0][0]
+
+        return render_template('results.html', price=price)
+
     return app
--- project/model.py
+++ project/model.py
@@ -1,21 +1,25 @@
+import pandas as pd
+import spacy
+from sklearn.feature_extraction.text import CountVectorizer
+import re
+
+
 def user_desc(text):
     """
     takes user description and makes it into lemmas
     """
-    
-    #spacy model
-    nlp = spacy.load('en_core_web_sm')
-    data = {'description': [text]}
-    text_df = pd.DataFrame(data = data)
-    
+    # Helper functions
     def clean_text(text):
         """
         takes text cleans it, and lemmatizes it using spacy
         """
-        text = str(text) #ensures text input as string
-        text = re.sub(r"[^a-zA-Z\s]", " ", text) #only letters of the alphabet and whitespaces. no numbers or punctuation.
-        text = re.sub(r"\s+", " ", text) #replaces multi-whitespaces with one whitespace.
-        text = text.lower().lstrip().rstrip() #removes whitespaces at the ends, and normalizes the letter case.
+        text = str(text)  # ensures text input as string
+        # only letters of the alphabet and whitespaces. no numbers or punctuation.
+        text = re.sub(r"[^a-zA-Z\s]", " ", text)
+        # replaces multi-whitespaces with one whitespace.
+        text = re.sub(r"\s+", " ", text)
+        # removes whitespaces at the ends, and normalizes the letter case.
+        text = text.lower().lstrip().rstrip()
         return text
 
     def lemmatize(text):
@@ -36,33 +40,38 @@ def user_desc(text):
         df['clean_text'] = df['description'].apply(clean_text)
         df['lemmas'] = df['clean_text'].apply(lemmatize)
         return df[['lemmas', 'clean_text']]
-    
-    #impact lemmas list
+
+    # impact lemmas list
     impact_lemmas = ['downtown',
-                 'private',
-                 'restaurant',
-                 'pool',
-                 'park',
-                 'clean',
-                 'coffee',
-                 'wifi',
-                 'patio',
-                 'backyard',
-                 'share',
-                 'free',
-                 'spacious']
-    
-    #nlp description stuff
+                     'private',
+                     'restaurant',
+                     'pool',
+                     'park',
+                     'clean',
+                     'coffee',
+                     'wifi',
+                     'patio',
+                     'backyard',
+                     'share',
+                     'free',
+                     'spacious']
+
+    # Spacy model
+    nlp = spacy.load('en_core_web_sm')
+    data = {'description': [text]}
+    text_df = pd.DataFrame(data=data)
+
+    # NLP description stuff
     mydf = clean_df(text_df)
     vect = CountVectorizer(tokenizer=lemmatize)
     vect.fit(mydf['clean_text'])
     dtm_tf = vect.transform(mydf['clean_text'])
-    dtm_tf = pd.DataFrame(dtm_tf.todense(), columns = vect.get_feature_names())
+    dtm_tf = pd.DataFrame(dtm_tf.todense(), columns=vect.get_feature_names())
     for lemma in impact_lemmas:
         if lemma in dtm_tf.columns:
             pass
         else:
             dtm_tf[lemma] = 0
     final_df = dtm_tf[impact_lemmas]
-    drop_columns = ['description', 'clean_text', 'lemmas']
+
     return final_df
